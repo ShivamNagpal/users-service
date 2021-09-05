2e971a4643f029037048426161642a91975fc79b
Shivam Nagpal <shivamnagpal432@gmail.com>
2021-09-06 01:39:52 +0530

    Divide the method in case the communication has to be direct

--- src/main/java/com/nagpal/shivam/workout_manager_user/dao/UserDao.java
+++ src/main/java/com/nagpal/shivam/workout_manager_user/dao/UserDao.java
@@ -1,12 +1,11 @@
 package com.nagpal.shivam.workout_manager_user.dao;
 
 import com.nagpal.shivam.workout_manager_user.dtos.ReplyExceptionMessage;
+import com.nagpal.shivam.workout_manager_user.exceptions.ResponseException;
 import com.nagpal.shivam.workout_manager_user.models.User;
-import com.nagpal.shivam.workout_manager_user.utils.Constants;
-import com.nagpal.shivam.workout_manager_user.utils.DbEventAddress;
-import com.nagpal.shivam.workout_manager_user.utils.DbUtils;
-import com.nagpal.shivam.workout_manager_user.utils.PgExceptionCodes;
+import com.nagpal.shivam.workout_manager_user.utils.*;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Future;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.Json;
 import io.vertx.ext.mongo.MongoClient;
@@ -14,7 +13,13 @@ import io.vertx.pgclient.PgException;
 import io.vertx.sqlclient.SqlClient;
 import io.vertx.sqlclient.Tuple;
 
+import java.util.Optional;
+
 public class UserDao {
+    public static final String INSERT_USER = "INSERT INTO \"user\"" +
+            "(first_name, last_name, email, \"password\", email_verified, account_status, meta, deleted, " +
+            "time_created, time_last_modified) " +
+            "VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id";
     private final EventBus eventBus;
     private final SqlClient sqlClient;
     private final MongoClient mongoClient;
@@ -34,40 +39,50 @@ public class UserDao {
     private void signUp() {
         eventBus.consumer(DbEventAddress.USER_SIGN_UP, message -> {
             User user = (User) message.body();
-            String sql = "INSERT INTO \"user\"" +
-                    "(first_name, last_name, email, \"password\", email_verified, account_status, meta, deleted, " +
-                    "time_created, time_last_modified) " +
-                    "VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id";
-            Tuple values = Tuple.of(
-                    user.getFirstName(),
-                    user.getLastName(),
-                    user.getEmail(),
-                    user.getPassword(),
-                    user.getEmailVerified(),
-                    user.getAccountStatus(),
-                    user.getMeta(),
-                    user.getDeleted(),
-                    user.getTimeCreated(),
-                    user.getTimeLastModified()
-            );
-            DbUtils.executeQueryAndReturnOne(sqlClient, sql, values, row -> row.getLong("id"))
+            signUp(user)
                     .onSuccess(message::reply)
                     .onFailure(throwable -> {
-                        if (throwable instanceof PgException) {
-                            PgException pgException = (PgException) throwable;
-                            if (pgException.getCode().equals(PgExceptionCodes.UNIQUE_KEY_CONSTRAINT_VIOLATION)) {
-                                ReplyExceptionMessage replyExceptionMessage =
-                                        new ReplyExceptionMessage(HttpResponseStatus.BAD_REQUEST.code(),
-                                                "User with " + pgException.getDetail(), null);
-                                message.fail(Constants.MESSAGE_FAILURE_HANDLED, Json.encode(
-                                        replyExceptionMessage));
-                            } else {
-                                message.fail(Constants.MESSAGE_FAILURE_UNHANDLED, pgException.getMessage());
-                            }
+                        if (throwable instanceof ResponseException) {
+                            ResponseException responseException = (ResponseException) throwable;
+                            ReplyExceptionMessage replyExceptionMessage =
+                                    new ReplyExceptionMessage(responseException.getStatus(),
+                                            responseException.getMessage(),
+                                            responseException.getPayload());
+                            message.fail(Constants.MESSAGE_FAILURE_HANDLED, Json.encode(
+                                    replyExceptionMessage));
                         } else {
                             message.fail(Constants.MESSAGE_FAILURE_UNHANDLED, throwable.getMessage());
                         }
                     });
         });
+    }
+
+    private Future<Long> signUp(User user) {
+        Tuple values = Tuple.of(
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getEmailVerified(),
+                user.getAccountStatus(),
+                user.getMeta(),
+                user.getDeleted(),
+                user.getTimeCreated(),
+                user.getTimeLastModified()
+        );
+        return DbUtils.executeQueryAndReturnOne(sqlClient, INSERT_USER, values, row -> row.getLong(ModelConstants.ID))
+                .map(Optional::get)
+                .recover(throwable -> {
+                    if (throwable instanceof PgException) {
+                        PgException pgException = (PgException) throwable;
+                        if (pgException.getCode().equals(PgExceptionCodes.UNIQUE_KEY_CONSTRAINT_VIOLATION)) {
+                            ResponseException responseException =
+                                    new ResponseException(HttpResponseStatus.BAD_REQUEST.code(),
+                                            "User with " + pgException.getDetail(), null);
+                            return Future.failedFuture(responseException);
+                        }
+                    }
+                    return Future.failedFuture(throwable);
+                });
     }
 }
